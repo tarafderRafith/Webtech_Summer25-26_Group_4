fa65172c39399a459aab95b3d48cb355c5226e19
404Shihab <shihabbhuiyan500@gmail.com>
2026-08-15 01:10:48 +0600

    initial test commit

---
+++ View/homel.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>UniResolve | University Complaint Management System</title>
+</head>
+<body>
+    <h1>UniResolve</h1>
+    <p>A University Complaint Management System</p>
+</body>
+</html>
