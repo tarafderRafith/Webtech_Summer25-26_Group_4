604302904b157f23be0526dc45b42dc36d1503dc
404Shihab <shihabbhuiyan500@gmail.com>
2026-08-15 01:23:27 +0600

    refactor home.php to use a variable for the title

--- View/home.php
+++ View/home.php
@@ -1,11 +1,17 @@
+<?php
+    $title = "UniResolve";
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>UniResolve | University Complaint Management System</title>
+    <title><?php echo $title; ?> | University Complaint Management System</title>
 </head>
 <body>
-    <h1>UniResolve</h1>
+
+    <h1><?php echo $title; ?></h1>
     <p>A University Complaint Management System</p>
+
 </body>
 </html>
